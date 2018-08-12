5a0ecd7aa4e48436b58746b57ff466852ccf1817
루트 <@DATACTR5-W64ADM>
2018-08-13 06:57:57 +0900

    보류

--- 문제/좋은수열/소스.cpp
+++ 문제/좋은수열/소스.cpp
@@ -1,10 +1,50 @@
 //https://www.acmicpc.net/problem/2661
 #include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
 using namespace std;
 
+vector<list<vertex>> adjacentList;
+int n; //N은 1 이상 80 이하이다.
+int idx = 0;
+class vertex {
+public:
+	vector<int> arr;
+	int index;
+	vertex(int i) {
+		arr = vector<int>();
+		arr.push_back(i);
+		index = ::idx++;
+	}
+	~vertex() {
+	}
+};
+
+void makeAdjacentList(int i) {
+	vector<bool> visited(0);
+	adjacentList[0].push_back(vertex(0));
+	adjacentList[1].push_back(vertex(1));
+	adjacentList[1].push_back(vertex(2));
+	adjacentList[1].push_back(vertex(3));
+
+	stack<int> st;
+	st.push(i);
+
+	while (!st.empty()) {
+		i = st.top(); st.pop();
+
+
+
+	}
+
+	
+}
+
 int main() {
-	int n; //N은 1 이상 80 이하이다.
+	
 	cin >> n;
+
 
 
 	return 0;
