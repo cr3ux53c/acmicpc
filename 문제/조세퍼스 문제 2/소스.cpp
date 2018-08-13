505059a2a9ed388ed7e40e879bb71361f2b08a64
루트 <@DATACTR5-W64ADM>
2018-08-13 13:00:58 +0900

    성공

---
+++ 문제/조세퍼스 문제 2/소스.cpp
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/1158
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	//(1 ¡Â M ¡Â N ¡Â 1,000)
+	int n, m;
+	cin >> n >> m;
+	queue<int> q;
+
+	return 0;
+}
