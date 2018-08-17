7d7b882109d24687c66e133208c7ba9416efd943
루트 <@DATACTR5-W64ADM>
2018-08-18 00:12:43 +0900

    성공

--- 문제/test/소스.cpp
+++ 문제/test/소스.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
-	
+	cout << floor(5 / 2.0 + 0.5);
+	cout << floor(4 / 2.0 + 0.5);
 	return 0;
 }
--- 문제/회전하는 큐/main.cpp
+++ 문제/회전하는 큐/main.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
 #include <string>
+#include <deque>
+#include <queue>
+#include <cmath>
 using namespace std;
 
 int main() {
-	
+	int n, m; // 1 <= m <= n <= 50
+	cin >> n >> m;
+
+	deque<int> d;
+	queue<int> q;
+
+	for (int i = 0; i < m; i++) {
+		int input;
+		cin >> input;
+		q.push(input);
+	}
+
+	for (int i = 1; i <= n; i++)
+		d.push_back(i);
+
+	int count = 0;
+
+	while (!q.empty()) {
+		int index;
+		for (int i = 0; i < d.size(); i++) {
+			if (q.front()==d[i]){
+				index = i+1;
+			}
+		}
+		if (index <= floor(d.size()/2.0+0.5)) {
+			while (q.front() != d.front()) {
+				d.push_back(d.front());
+				d.pop_front();
+				count++;
+			}
+			d.pop_front();
+			q.pop();
+		} else {
+			while (q.front() != d.front()) {
+				d.push_front(d.back());
+				d.pop_back();
+				count++;
+			}
+			d.pop_front();
+			q.pop();
+		}
+	}
+	cout << count;
 	return 0;
 }
