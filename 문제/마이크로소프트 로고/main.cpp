6d7e9d264fafa0e76b36524fc9edbdd2323026b1
루트 <@DATACTR5-W64ADM>
2018-08-23 02:35:57 +0900

    성공

---
+++ 문제/마이크로소프트 로고/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "       _.-;;-._\n";
+	cout << "\'-..-\'|   ||   |\n";
+	cout << "\'-..-\'|_.-;;-._| \n";
+	cout << "\'-..-\'|   ||   |\n";
+	cout << "\'-..-\'|_.-\'\'-._|\n";
+	return 0;
+}
