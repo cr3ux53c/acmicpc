36bb2d5e1c35c0051f1c86f5a8a383548f6dc601
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-09-15 13:19:25 +0900

    보류

--- 문제/계단 오르기/main.cpp
+++ 문제/계단 오르기/main.cpp
@@ -2,25 +2,25 @@
 #pragma warning (disable:4996)
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
 
-int map[1001][3];
+int arr[300];
 
 int main(int n, char *argv[]) {
 	//init
-	int result = 999999;
+	int result = 0;
 	//input
 	cin >> n;
-	for (int i = 1; i <= n; i++)
-		scanf("%d %d %d", &map[i][0], &map[i][1], &map[i][2]);
-	//calc
-	for (int i = 1; i <= n; i++)
-		for (int j = 0; j < 3; j++)
-			map[i][j] += map[i - 1][(j + 1) % 3] > map[i - 1][(j + 2) % 3] ? map[i - 1][(j + 2) % 3] : map[i - 1][(j + 1) % 3];
-	for (int i = 0; i < 3; i++)
-		result = map[n][i] > result ? result : map[n][i];
-	cout << result;
+	for (int i = 0; i < n; i++) {
+		int input; cin >> input;
+		arr[i] = input;
+	}
 
+	//calc
+	for (int i = 0; i < n; i++) {
+
+	}
+	
+	cout << result;
 	return 0;
 }
