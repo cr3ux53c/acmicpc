b87ebb5933efe5ea6745a638b35ee6901f8d829c
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-09-23 17:42:03 +0900

    성공

--- 문제/크로아티아 알파벳/main.cpp
+++ 문제/크로아티아 알파벳/main.cpp
@@ -1,11 +1,31 @@
 //https://www.acmicpc.net/problem/2941
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main(int count, char *argv[]) {
-	count = 0;
-	string s; cin >> s;
+vector<string> croatia = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
+int main(int count, char *argv[]) {
+	string s; cin >> s;
+	count = s.size();
+
+	for (int i = 0; i < s.size(); i++) {
+		for (int j = 0; j < croatia.size(); j++) {
+			if (s[i] == croatia[j][0] && s[i+1] == croatia[j][1]) {
+				if (j == 2) {
+					if (s[i+2] == croatia[j][2]) {
+						count -= 2;
+						i += 2;
+					}
+					continue;
+				}
+				count--;
+				i++;
+			}
+		}
+	}
+
+	cout << count;
 	return 0;
 }
