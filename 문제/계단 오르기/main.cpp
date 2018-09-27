9a2d5e7665dbeff41c5ba76bad071b6c57232b1f
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-09-28 00:22:52 +0900

    성공 (나중에 다시 시도)

--- 문제/계단 오르기/main.cpp
+++ 문제/계단 오르기/main.cpp
@@ -5,34 +5,22 @@
 #include <vector>
 using namespace std;
 
-int maxResult = -1;
-
-int jump(vector<int>& arr, int prevStair, int currentStair) {
-	int sum = 0;
-	//baseCase
-	if (currentStair < 0)
-		return 0;
-
-	//calc
-	if (prevStair != currentStair + 1) // 두번 연속 계단이 아니면
-		return arr[currentStair] + jump(arr, currentStair, currentStair - 1);
-	return arr[currentStair] + jump(arr, currentStair, currentStair - 2);
-
-
-}
-
-
 int main(int n, char *argv[]) {
 	//input
 	cin >> n;
-	vector<int> arr(n, -1);
-	for (int i = 0; i < n; i++) {
-		int input; cin >> input;
-		arr[i] = input;
-	}
+	vector<int> stair(n, -1);
+	vector<int> dp(n, -1);
+	for (int i = 0; i < n; i++)
+		cin >> stair[i];
 
 	//calc
-	cout << jump(arr, n - 1, n - 1);
+	dp[0] = stair[0];
+	dp[1] = (stair[0] + stair[1] > stair[1]) ? stair[0] + stair[1] : stair[1];
+	dp[2] = (stair[0] + stair[2] > stair[1] + stair[2]) ? stair[0] + stair[2] : stair[1] + stair[2];
+	
+	for (int i = 3; i < n; i++)
+		dp[i] = (dp[i - 2] + stair[i] > stair[i - 1] + stair[i] + dp[i - 3]) ? dp[i - 2] + stair[i] : stair[i - 1] + stair[i] + dp[i - 3];
 
+	cout << dp[n - 1];
 	return 0;
 }
