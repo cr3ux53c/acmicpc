043d87520c4c87814da78f19a2ace06ceb0a6cb8
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-09-28 02:32:37 +0900

    나중에 다시 시도

--- 문제/ACM Craft/main.cpp
+++ 문제/ACM Craft/main.cpp
@@ -1,18 +1,52 @@
 //https://www.acmicpc.net/problem/1005
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main(int t, char *argv[]) {
+vector<int> buildTime;
+vector<int> dp;
+vector<vector<int>> order;
+int n, k, target;
+
+int getMax(int target, int b) {
+	return target > b ? target : b;
+}
+
+int calc(int target) {
+	if (dp[target] != 0) {
+		return dp[target];
+	} else {
+		int result = 0;
+		for (int i = 1; i <= n; i++) {
+			if (order[target][i] == 1) {
+				result = getMax(result, calc(i));
+			}
+		}
+		return dp[target] = result + buildTime[target];
+	}
+}
+
+int main(int t) {
 	cin >> t;
 
 	//testCase
 	for (int i = 0; i < t; i++) {
-		int n, k; cin >> n >> k;
+		buildTime = vector<int>(1001, 0);
+		dp = vector<int>(1001, 0);
+		order = vector<vector<int>>(1001, vector<int>(1001));
 
+		//input
+		cin >> n >> k;
+		for (int i = 1; i <= n; i++)
+			cin >> buildTime[i];
+		for (int i = 0; i < k; i++) {
+			int target, b; cin >> target >> b;
+			order[b][target] = 1;
+		}
+		cin >> target;
 
+		cout << calc(target) << '\n';
 	}
-
-
 	return 0;
 }
