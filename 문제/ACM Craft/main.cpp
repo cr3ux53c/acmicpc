79f0a80252425913e41dd3d6cf9948f1afd94478
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-09-28 02:07:24 +0900

    보류

---
+++ 문제/ACM Craft/main.cpp
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/1005
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int t, char *argv[]) {
+	cin >> t;
+
+	//testCase
+	for (int i = 0; i < t; i++) {
+		int n, k; cin >> n >> k;
+
+
+	}
+
+
+	return 0;
+}
