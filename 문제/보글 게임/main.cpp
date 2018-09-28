6de79c078ac7e7075e63e697a72ae9a23663ba8d
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-09-28 12:21:03 +0900

    보류

--- 문제/보글 게임/main.cpp
+++ 문제/보글 게임/main.cpp
@@ -1,4 +1,6 @@
+//https://algospot.com/judge/problem/read/BOGGLE
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 
@@ -6,32 +8,51 @@ using namespace std;
 //const int dy[8] = { -1, 0, 1, -1, 0, 1, -1, 1 };
 const int dx[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 const int dy[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+char matrix[5][5];
 
-char matrix[][6] = {
-	"URLPM",
-	"XPRET",
-	"GIAET",
-	"XTNZY",
-	"XOQRS",
-};
-
-bool hasWord(int y, int x, char* word) {
+bool hasWord(int y, int x, string word) {
 	//baseCase
 	if (word[0] != matrix[y][x])
 		return false;
-	if (strlen(word) == 1) {
+	if (word.size() == 1) {
 		cout << "TRUE";
 		return true;
 	}
 	//calc
+	bool isNext = false;
 	for (int i = 0; i < 8; i++) {
-		if (hasWord(y + dy[i], x + dx[i], word + 1)) {
+		if (hasWord(y + dy[i], x + dx[i], word.substr(1, 1))) {
+			isNext = true;
 		}
+	}
+	if (isNext) {
+
 	}
 }
 
-int main(int argc, char *argv[]) {
-	char word[] = "ENA";
-	hasWord(2, 3, word);
+int main(int t, char *argv[]) {
+	cin >> t;
+	//testCase
+	for (int i = 0; i < t; i++) {
+		vector<string> word;
+		//input
+		for (int j = 0; j < 5; j++)
+			for (int k = 0; k < 5; k++)
+				cin >> matrix[j][k];
+		int n; cin >> n;
+		word = vector<string>(n);
+		for (int j = 0; j < n; j++)
+			cin >> word[j];
+		
+		//calc
+		for (auto s : word) {
+			for (int j = 0; j < 5; j++) {
+				for (int k = 0; k < 5; k++) {
+					if (matrix[j][k] == s[0])
+						cout << s << (hasWord(j, k, s) ? "YES" : "NO") << "\n";
+				}
+			}
+		}
+	}
 	return 0;
 }
