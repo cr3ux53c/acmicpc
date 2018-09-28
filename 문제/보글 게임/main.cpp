6d14b6f97c7320510a4482f5446a4d6fd35b6f39
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-09-28 19:53:18 +0900

    PANDORA가 안되네

--- 문제/보글 게임/main.cpp
+++ 문제/보글 게임/main.cpp
@@ -4,8 +4,6 @@
 #include <string>
 using namespace std;
 
-//const int dx[8] = { -1, -1, -1, 1, 1, 1, 0, 0 };
-//const int dy[8] = { -1, 0, 1, -1, 0, 1, -1, 1 };
 const int dx[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 const int dy[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 char matrix[5][5];
@@ -15,18 +13,14 @@ bool hasWord(int y, int x, string word) {
 	if (word[0] != matrix[y][x])
 		return false;
 	if (word.size() == 1) {
-		cout << "TRUE";
 		return true;
 	}
 	//calc
 	bool isNext = false;
 	for (int i = 0; i < 8; i++) {
-		if (hasWord(y + dy[i], x + dx[i], word.substr(1, 1))) {
-			isNext = true;
+		if (hasWord(y + dy[i], x + dx[i], word.substr(1, word.size()))) {
+			return true;
 		}
-	}
-	if (isNext) {
-
 	}
 }
 
@@ -34,24 +28,27 @@ int main(int t, char *argv[]) {
 	cin >> t;
 	//testCase
 	for (int i = 0; i < t; i++) {
-		vector<string> word;
+		vector<string> words;
 		//input
 		for (int j = 0; j < 5; j++)
 			for (int k = 0; k < 5; k++)
 				cin >> matrix[j][k];
 		int n; cin >> n;
-		word = vector<string>(n);
+		words = vector<string>(n);
 		for (int j = 0; j < n; j++)
-			cin >> word[j];
+			cin >> words[j];
 		
 		//calc
-		for (auto s : word) {
+		for (auto s : words) {
+			//¸Ê ÀüÃ¼ °Ë»ö
+			bool isYes = false;
 			for (int j = 0; j < 5; j++) {
 				for (int k = 0; k < 5; k++) {
-					if (matrix[j][k] == s[0])
-						cout << s << (hasWord(j, k, s) ? "YES" : "NO") << "\n";
+					if (matrix[j][k] == s[0] && hasWord(j, k, s))
+						isYes = true;
 				}
 			}
+			cout << s << " " << (isYes ? "YES" : "NO") << "\n";
 		}
 	}
 	return 0;
