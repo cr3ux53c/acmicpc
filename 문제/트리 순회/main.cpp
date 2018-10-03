8dd0c151f99b63a95bc2d3febdbe3e29ba7ceb4c
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-10-04 06:10:44 +0900

    개같다 증말

--- 문제/트리 순회/main.cpp
+++ 문제/트리 순회/main.cpp
@@ -4,29 +4,54 @@
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <set>
+#include <map>
+#include <stack>
 using namespace std;
 
 int n;
 vector<pair<char, pair<char, char>>> matrix;
+map<char, pair<char, char>> m;
 
-//void preorderTraversal(int index) {
-//	auto edge = matrix[index];
-//	for (auto it = edge.begin(); it != edge.end(); it++) {
-//		if (it == edge.begin()) {
-//			cout << *it;
-//		} else if (*it != '.') {
-//			int nextIndex;
-//			for (int i = 0; i < n; i++) {
-//				if (*matrix[i].begin() == *it){
-//					nextIndex = i;
-//					break;
-//				}
-//			}
-//			preorderTraversal(nextIndex);
-//		} else
-//			continue;
-//	}
-//}
+
+void preorderTraversal(int index) {
+	auto i = m['A'];
+	stack<char> st;
+	st.push('A');
+	while (!st.empty()) {
+		cout << st.top(); st.pop();
+		if (i.first != '.') {
+			cout << i.first;
+			if (i.second != '.') st.push(i.second);
+			i = m[i.first];
+			continue;
+		}
+		if (i.second != '.') {
+			cout << i.second;
+			i = m[i.second];
+			continue;
+		}
+		i = m[st.top()]; st.pop();
+	}
+	
+	//
+	//auto edge = matrix[index];
+	//for (auto it = edge.begin(); it != edge.end(); it++) {
+	//	if (it == edge.begin()) {
+	//		cout << *it;
+	//	} else if (*it != '.') {
+	//		int nextIndex;
+	//		for (int i = 0; i < n; i++) {
+	//			if (*matrix[i].begin() == *it){
+	//				nextIndex = i;
+	//				break;
+	//			}
+	//		}
+	//		preorderTraversal(nextIndex);
+	//	} else
+	//		continue;
+	//}
+}
 //
 //void inorderTraversal(int index) {
 //	auto edge = matrix[index];
@@ -51,21 +76,20 @@ int main(int argc, char* argv[]) {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
 	//input
+	
 	pair<char, char> a = pair<char, char>();
 	cin >> n;
 	matrix = vector<pair<char, pair<char, char>>>(n, pair<char, pair<char, char>>());
 	for (int i = 0; i < n; i++) {
-		pair<char, pair<char, char>> input;
 		char ch[3];
 		for (int j = 0; j < 3; j++)
 			cin >> ch[j];
-		input.first = ch[0];
-		input.second = pair<char, char>(ch[1], ch[2]);
-		matrix[i] = input;
+		m[ch[0]].first = ch[1];
+		m[ch[0]].second = ch[2];
 	}
 
 	//calc
-	//preorderTraversal(0);
+	preorderTraversal(0);
 	cout << "\n";
 	
 	return 0;
