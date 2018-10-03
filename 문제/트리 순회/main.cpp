41535752a2b32825b0d7ab83e3eb4ffc14a0881a
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-10-04 02:46:27 +0900

    보류

---
+++ 문제/트리 순회/main.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/1991
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char* argv[]) { 
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	return 0;
+}
