4421ac66887f4df73f1a5fb578f818ebe05d4d44
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-10-04 05:29:01 +0900

    Revert "map, set은 아닌가바"
    This reverts commit 28b68ab0230bd3f8da0b577793f30fb38df64d15.

--- 문제/트리 순회/main.cpp
+++ 문제/트리 순회/main.cpp
@@ -4,9 +4,6 @@
 #include <vector>
 #include <list>
 #include <algorithm>
-#include <stack>
-#include <map>
-#include <set>
 using namespace std;
 
 int n;
@@ -56,77 +53,6 @@ int main(int argc, char* argv[]) {
 	//input
 	cin >> n;
 	matrix = vector<list<char>>(n, list<char>(3));
-
-	set<int> edge;
-	vector<set<char>> mat(n, set<char>());
-	map<char, pair<char, char>> m;
-	pair<char, char> p;
-	vector<char> parent;
-	parent.push_back('R');
-	parent.push_back('A');
-	parent.push_back('B');
-	parent.push_back('C');
-	parent.push_back('E');
-	parent.push_back('F');
-	parent.push_back('D');
-
-	p.first = 'B';
-	p.second = 'C';
-	m['A'] = p;
-	p.first = 'D';
-	p.second = '.';
-	m['B'] = p;
-	p.first = 'E';
-	p.second = 'F';
-	m['C'] = p;
-	p.first = '.';
-	p.second = '.';
-	m['E'] = p;
-	p.first = '.';
-	p.second = '.';
-	m['F'] = p;
-	p.first = '.';
-	p.second = '.';
-	m['D'] = p;
-
-	stack<map<char, pair<char, char>>::iterator> st;
-	
-	do {
-		if (m[0].first != 'A') {
-
-		}
-
-	} while (st.empty() != NULL);
-
-	do {
-
-		for (auto it = m.begin(); it != m.end(); it++) {
-			if ((*it).first != '.') {
-				st.push(it);
-				it = m[(*it).first]
-			}
-		}
-		
-	} while (st.empty() != NULL);
-
-
-
-	stack<map<char, pair<char, char>>> st;
-	st.push(m);
-
-	while (!st.empty()) {
-		if (m['R'].first != '.') {
-
-		} else {
-			
-		}
-	}
-
-
-
-
-	
-
 	for (int i = 0; i < n; i++) {
 		list<char> input;
 		for (int j = 0; j < 3; j++) {
@@ -139,6 +65,6 @@ int main(int argc, char* argv[]) {
 	//calc
 	preorderTraversal(0);
 	cout << "\n";
-	pair<int, int> a;
+	
 	return 0;
 }
