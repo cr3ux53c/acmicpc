14ea444a0501e6711eb32e1ec9707aa7247286d9
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-10-05 05:17:12 +0900

    보류

---
+++ 문제/줄 세우기/main.cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/2252
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <list>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n, m; cin >> n >> m;
+	list<int> order(n);
+
+	int i = 1;
+	for (auto it = order.begin(); it != order.end(); it++) {
+		*it = i++;
+	}
+
+	for (int i = 0; i < m; i++) {
+		int a, b; cin >> a >> b;
+		if (distance(order.begin(), find(order.begin(), order.end(), a)) > distance(order.begin(), find(order.begin(), order.end(), b))) {
+			order.insert(order.begin(), a);
+		}
+		
+	}
+
+
+	return 0;
+}
