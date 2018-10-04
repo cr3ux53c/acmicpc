a3f5e2c30e27e6fa8f697f682720e2e6404c5d01
루트@W63A61 <cr3ux53c@gmail.com>
2018-10-04 16:16:54 +0900

    DP 적용 중

---
+++ 문제/미로 탐색/main.cpp
@@ -0,0 +1,54 @@
+//https://www.acmicpc.net/problem/2178
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+enum XY { Y, X };
+const int D[][4] = {
+//	  동,서,남,북
+	{ 0, 0, 1, -1 },
+	{ 1, -1, 0, 0} ,
+};
+
+int main(int argc, char* argv[]) { 
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	//input
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> map(n, vector<int>(m));
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		for (int j = 0; j < s.size(); j++)
+			map[i][j] = s[j] - '0';
+	}
+
+	//calc
+	stack<pair<int, int>> st;
+	st.push({ 0, 0 });
+	while (st.empty() == false) {
+		auto pos = st.top(); st.pop();
+		//동서남북
+		for (int i = 0; i < 4; i++) {
+			//Overflow check
+			if (pos.first + D[Y][i] < 0 || pos.first + D[Y][i] >= n)
+				continue;
+			if (pos.second + D[X][i] < 0 || pos.second + D[X][i] >= n)
+				continue;
+			if (map[pos.first+ D[Y][i]][pos.second+ D[X][i]] > 1) { // 다음 좌표가 1 초과면
+				map[pos.first + D[Y][i]][pos.second + D[X][i]] = min(map[pos.first + D[Y][i]][pos.second + D[X][i]], map[pos.first][pos.second] +1);
+				st.push({ pos.first + D[Y][i], pos.second + D[X][i] });
+			} else if (map[pos.first + D[Y][i]][pos.second + D[X][i]] == 1){
+				map[pos.first + D[Y][i]][pos.second + D[X][i]] += map[pos.first][pos.second];
+				st.push({ pos.first + D[Y][i], pos.second + D[X][i] });
+			} else {
+				continue;
+			}
+		}
+	}
+
+	return 0;
+}
