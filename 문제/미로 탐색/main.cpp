48b842eb597383a94c996b11a4c8cd94a7b6c67d
루트@W63A61 <cr3ux53c@gmail.com>
2018-10-04 20:19:54 +0900

    1차 성공

--- 문제/미로 탐색/main.cpp
+++ 문제/미로 탐색/main.cpp
@@ -31,23 +31,29 @@ int main(int argc, char* argv[]) {
 	st.push({ 0, 0 });
 	while (st.empty() == false) {
 		auto pos = st.top(); st.pop();
+
+		if (pos.first == n-1 && pos.second == m-1) {
+			cout << map[pos.first][pos.second];
+			break;
+		}
+
 		//동서남북
 		for (int i = 0; i < 4; i++) {
 			//Overflow check
 			if (pos.first + D[Y][i] < 0 || pos.first + D[Y][i] >= n)
 				continue;
-			if (pos.second + D[X][i] < 0 || pos.second + D[X][i] >= n)
+			if (pos.second + D[X][i] < 0 || pos.second + D[X][i] >= m)
+				continue;
+			if (map[pos.first + D[Y][i]][pos.second + D[X][i]] == 0)
 				continue;
 			if (map[pos.first+ D[Y][i]][pos.second+ D[X][i]] > 1) { // 다음 좌표가 1 초과면
 				map[pos.first + D[Y][i]][pos.second + D[X][i]] = min(map[pos.first + D[Y][i]][pos.second + D[X][i]], map[pos.first][pos.second] +1);
-				st.push({ pos.first + D[Y][i], pos.second + D[X][i] });
 			} else if (map[pos.first + D[Y][i]][pos.second + D[X][i]] == 1){
 				map[pos.first + D[Y][i]][pos.second + D[X][i]] += map[pos.first][pos.second];
-				st.push({ pos.first + D[Y][i], pos.second + D[X][i] });
-			} else {
-				continue;
 			}
+			st.push({ pos.first + D[Y][i], pos.second+ D[X][i] });
 		}
+		map[pos.first][pos.second] = 0;
 	}
 
 	return 0;
