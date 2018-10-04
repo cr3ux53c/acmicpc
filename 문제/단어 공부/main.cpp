f9be0baa437a0937a2eb00b93bbd03a1874a6d4c
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-10-05 03:41:32 +0900

    성공

--- 문제/단어 공부/main.cpp
+++ 문제/단어 공부/main.cpp
@@ -3,31 +3,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
-#include <set>
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	string s; cin >> s;
 	vector<int> map(26, 0);
 	
+	//input
 	for (char ch : s) {
 		int i = toupper(ch);
 		map[i-65]++;
 	}
 	
-	int maxNum = -1;
-	int maxIndex = -1;
-	for (int i = 0; i < map.size();i++) {
-		if (map[i] > maxNum && map[i] > 0) { 
-			maxNum = map[i];
-			maxIndex = i;
-		} else if (map[i] == maxNum) {
-			cout << '?';
-			return 0;
+	//calc
+	char result = '?';
+	pair<char, int> count = { 'A', 0 };
+	for (int i = 0; i < map.size(); i++) {
+		if (map[i] > count.second) {
+			result = count.first = i + 65;
+			count.second = map[i];
+		} else if (map[i] == count.second) {
+			result = '?';
 		}
 	}
-	cout << (char)(maxIndex+65);
+
+	cout << result;
 	return 0;
 }
 
