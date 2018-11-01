96a6c257ac3cee14142214e7a7a28f851c805075
CR3UX53C <cr3ux53c@gmail.com>
2018-11-01 14:46:22 +0900

    왜 안되

--- 문제/최소값 찾기/main.cpp
+++ 문제/최소값 찾기/main.cpp
@@ -11,35 +11,26 @@ int main(int argc, char* argv[]) {
 	
 	//input
 	int N, L; cin >> N >> L;
-	vector<int> vec(N);
-	vector<int> D(N);
 	deque<int> dq;
 
-	for (int offset = 0;offset<N;offset++) {
-		int start = max(offset - L + 1, 0);
-		int end = offset + 1;
+	int start, end, result;
 
-		//누적 입력
-		/*for (int i = start; i < end; i++) {
-			int input; cin >> input;
-			dq.push_back(input);
-		}*/
+	for (int offset = 0;offset<N;offset++) {
+		start = max(offset - L + 1, 0);
+		end = offset + 1;
+
 		int input; cin >> input;
 		dq.push_back(input);
 		
-		cout << *min_element(dq.begin()+start, dq.end()) << " ";
+		result = INT32_MAX;
+		for (auto it = dq.begin(); it != dq.end(); it++)
+			result = min(result, *it);
+
+		cout << result << " ";
+
+		if (offset - L + 1 >= 0)
+			dq.pop_front();
 	}
-
-
-	////calc
-	//for (int i = 0; i < N; i++) {
-	//	//get Di
-	//	//int offset = max(i - L + 1, 0);
-	//	/*for (auto it = vec.begin() + max(i - L + 1, 0);it!=vec.end()-(N-i-1);it++){
-	//		cout << *it << " ";
-	//	}*/
-	//	cout << *min_element(vec.begin() + max(i - L + 1, 0), vec.end() - (N - i - 1)) << " ";
-	//}
 
 	return 0;
 }
