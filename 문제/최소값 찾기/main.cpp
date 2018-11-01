ad0ae2b560a1d9693a4e2247c3f1efd3edf267f9
CR3UX53C <cr3ux53c@gmail.com>
2018-11-01 14:28:15 +0900

    누적 입력해도 시간초과

--- 문제/최소값 찾기/main.cpp
+++ 문제/최소값 찾기/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <deque>
 #include <algorithm>
 using namespace std;
 
@@ -12,20 +13,33 @@ int main(int argc, char* argv[]) {
 	int N, L; cin >> N >> L;
 	vector<int> vec(N);
 	vector<int> D(N);
+	deque<int> dq;
 
-	for (int i = 0; i < N; i++) {
-		cin >> vec[i];
-	}
+	for (int offset = 0;offset<N;offset++) {
+		int start = max(offset - L + 1, 0);
+		int end = offset + 1;
 
-	//calc
-	for (int i = 0; i < N; i++) {
-		//get Di
-		//int offset = max(i - L + 1, 0);
-		/*for (auto it = vec.begin() + max(i - L + 1, 0);it!=vec.end()-(N-i-1);it++){
-			cout << *it << " ";
+		//누적 입력
+		/*for (int i = start; i < end; i++) {
+			int input; cin >> input;
+			dq.push_back(input);
 		}*/
-		cout << *min_element(vec.begin() + max(i - L + 1, 0), vec.end() - (N - i - 1)) << " ";
+		int input; cin >> input;
+		dq.push_back(input);
+		
+		cout << *min_element(dq.begin()+start, dq.end()) << " ";
 	}
+
+
+	////calc
+	//for (int i = 0; i < N; i++) {
+	//	//get Di
+	//	//int offset = max(i - L + 1, 0);
+	//	/*for (auto it = vec.begin() + max(i - L + 1, 0);it!=vec.end()-(N-i-1);it++){
+	//		cout << *it << " ";
+	//	}*/
+	//	cout << *min_element(vec.begin() + max(i - L + 1, 0), vec.end() - (N - i - 1)) << " ";
+	//}
 
 	return 0;
 }
