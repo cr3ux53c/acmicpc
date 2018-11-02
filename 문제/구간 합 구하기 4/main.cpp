b4d22b2748a8c7f5778a99d7f7c2816f1d6bff6f
표준 개발 환경@W63A61 <cr3ux53c@gmail.com>
2018-11-02 14:33:08 +0900

    리펙토링

--- 문제/구간 합 구하기 4/main.cpp
+++ 문제/구간 합 구하기 4/main.cpp
@@ -10,29 +10,17 @@ int main(int argc, char* argv[]) {
 	
 	//input
 	int n, m; cin >> n >> m;
-	vector<int> vec(n);
 	vector<int> prefixSum(n);
-	for (int i = 0; i < n; i++) {
-		int input; cin >> input;
-		vec[i] = input;
-	}
-
-	//calc
-		//prefix sum
-	prefixSum[0] = vec[0];
+	cin >> prefixSum[0];
 	for (int i = 1; i < n; i++) {
-		prefixSum[i] = prefixSum[i-1] + vec[i];
+		int input; cin >> input;
+		prefixSum[i] = prefixSum[i - 1] + input;
 	}
 
 	//calc
 	for (int i = 0; i < m; i++) {
 		int start, end; cin >> start >> end;
-		int operandEnd;
-		if (start - 2 < 0)
-			operandEnd = 0;
-		else
-			operandEnd = prefixSum[start - 2];
-		cout << prefixSum[end - 1] - operandEnd << '\n';
+		cout << prefixSum[end - 1] - (start - 2 < 0 ? 0 : prefixSum[start - 2]) << '\n';
 	}
 
 	return 0;
