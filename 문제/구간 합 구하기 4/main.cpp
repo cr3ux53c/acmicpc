da427822eabeee6ca6f49ce1d45973acad5a6729
표준 개발 환경@W63A61 <cr3ux53c@gmail.com>
2018-11-02 14:02:46 +0900

    성공 - prefixSum

--- 문제/구간 합 구하기 4/main.cpp
+++ 문제/구간 합 구하기 4/main.cpp
@@ -11,19 +11,28 @@ int main(int argc, char* argv[]) {
 	//input
 	int n, m; cin >> n >> m;
 	vector<int> vec(n);
+	vector<int> prefixSum(n);
 	for (int i = 0; i < n; i++) {
 		int input; cin >> input;
 		vec[i] = input;
 	}
 
 	//calc
+		//prefix sum
+	prefixSum[0] = vec[0];
+	for (int i = 1; i < n; i++) {
+		prefixSum[i] = prefixSum[i-1] + vec[i];
+	}
+
+	//calc
 	for (int i = 0; i < m; i++) {
 		int start, end; cin >> start >> end;
-		int sum = 0;
-
-		for (auto it = vec.begin() + start - 1; it != vec.end()-n+end; it++)
-			sum += *it;
-		cout << sum << '\n';
+		int operandEnd;
+		if (start - 2 < 0)
+			operandEnd = 0;
+		else
+			operandEnd = prefixSum[start - 2];
+		cout << prefixSum[end - 1] - operandEnd << '\n';
 	}
 
 	return 0;
