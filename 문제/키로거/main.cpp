8243badd73d298505443ac817ab4520ae0d1b98b
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2018-12-27 07:31:49 +0900

    런타임 에러

---
+++ 문제/키로거/main.cpp
@@ -0,0 +1,41 @@
+//https://www.acmicpc.net/problem/5397
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+using namespace std;
+
+int main(int testCases, char* argv[]) {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> testCases;
+
+	for (int i = 0; i < testCases; i++) {
+		string input; cin >> input;
+		string output = "";
+		int pos = 0;
+		
+		for (int i = 0; i < input.size(); i++) {
+			switch (input[i]) {
+				case '<':
+					if (pos > 0) pos--;
+					break;
+				case '>':
+					if (pos < output.size()) pos++;
+					break;
+				case '-':
+					if (pos > 0) {
+						output.erase(pos-1, 1);
+					}
+					break;
+				default:
+					output.insert(pos++, input.substr(i, 1));
+					break;
+			}
+		}
+		cout << output << '\n';
+	}
+
+	return 0;
+}
