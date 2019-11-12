e58e8e3c895ac4b17fce20d097a442c4fc84b6a8
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-11-12 18:18:07 +0900

    고양이 해결

---
+++ 문제/고양이/소스.cpp
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/10171
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+	return 0;
+}
