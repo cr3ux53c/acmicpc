5c85b176b0f267574363e7aa171ecd266c8ee234
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-11-17 20:18:42 +0900

    IDE 변경

---
+++ 문제/A+B - 3-py3/A_B___3_py3.py
@@ -0,0 +1,10 @@
+#https://www.acmicpc.net/problem/10950
+
+test_cases = int(input())
+
+for i in range(test_cases):
+    input = input()
+    
+    A = int(input())
+    B = int(input())
+    print(A+B)
